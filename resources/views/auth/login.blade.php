5bd419685f1b5b9475ba20a2b2c156dfd6caee68
Riteable <contact@riteable.com>
2021-02-10 16:13:30 +0100

    added card styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,75 +2,90 @@
 
 @section('content')
     <x-layout.container>
-        <x-alert message="{{ $errors->first() }}" type="error" />
+        <div class="columns justify-center">
+            <div class="column col-6">
+                <x-alert message="{{ $errors->first() }}" type="error" />
 
-        <form method="post" action="{{ route('login') }}">
-            @csrf
+                <div class="card">
+                    <header class="card-header">
+                        <h2 class="card-title h5">
+                            {{ __('Login') }}
+                        </h2>
+                        <p class="card-subtitle text-gray">
+                            {{ __('Sign in with an existing account.') }}
+                        </p>
+                    </header>
 
-            <div class="form-group">
-                <x-form.label
-                    text="{{ __('Email') }}"
-                    for="form-email"
-                />
+                    <div class="card-body">
+                        <form method="post" action="{{ route('login') }}">
+                            @csrf
 
-                <x-form.input
-                    id="form-email"
-                    type="email"
-                    name="email"
-                    value="{{ old('email') }}"
-                    autocomplete="email"
-                    placeholder="@"
-                    required
-                />
-            </div>
+                            <div class="form-group">
+                                <x-form.label
+                                    text="{{ __('Email') }}"
+                                    for="form-email"
+                                />
 
-            <div class="form-group">
-                <div class="d-flex space-between align-center">
-                    <x-form.label
-                        text="{{ __('Password') }}"
-                        for="form-password"
-                    />
+                                <x-form.input
+                                    id="form-email"
+                                    type="email"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    autocomplete="email"
+                                    placeholder="@"
+                                    required
+                                />
+                            </div>
 
-                    <div>
-                        <a href="/forgot-password">
-                            {{ __('Forgot password?') }}
+                            <div class="form-group">
+                                <div class="d-flex space-between align-center">
+                                    <x-form.label
+                                        text="{{ __('Password') }}"
+                                        for="form-password"
+                                    />
+
+                                    <div>
+                                        <a href="/forgot-password">
+                                            {{ __('Forgot password?') }}
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <x-form.input
+                                    id="form-password"
+                                    type="password"
+                                    name="password"
+                                    autocomplete="password"
+                                    placeholder="..."
+                                    required
+                                />
+                            </div>
+
+                            <div class="form-group">
+                                <x-form.label>
+                                    <x-form.checkbox
+                                        id="form-remember"
+                                        name="remember"
+                                        checked="{{ old('remember') }}"
+                                    />
+
+                                    {{ __('Remember me') }}
+                                </x-form.label>
+                            </div>
+
+                            <x-button text="{{ __('Login') }}" />
+                        </form>
+                    </div>
+
+                    <div class="card-footer">
+                        {{ __('Not registered?') }}
+
+                        <a href="{{ route('register') }}">
+                            {{ __('Register') }}
                         </a>
                     </div>
                 </div>
-
-                <x-form.input
-                    id="form-password"
-                    type="password"
-                    name="password"
-                    autocomplete="password"
-                    placeholder="..."
-                    required
-                />
             </div>
-
-            <div class="form-group">
-                <x-form.label>
-                    <x-form.checkbox
-                        id="form-remember"
-                        name="remember"
-                        checked="{{ old('remember') }}"
-                    />
-
-                    {{ __('Remember me') }}
-                </x-form.label>
-            </div>
-
-            <div class="d-flex space-between align-center">
-                <x-button text="{{ __('Login') }}" />
-
-                <span>
-                    {{ __('Not registered?') }}
-
-                    <a href="{{ route('register') }}">
-                        {{ __('Register') }}
-                    </a>
-                </span>
-            </div>
-        </form>
+        </div>
     </x-layout.container>
 @endsection
