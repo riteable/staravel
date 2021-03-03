62c1fec1b96e3da88124709a7ed7d09543a4ed40
Riteable <contact@riteable.com>
2021-03-03 11:24:23 +0100

    added container padding

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,68 +1,66 @@
 <header class="page-header">
-    <x-layout.container padding="0">
-        <nav class="navbar">
-            <div class="navbar-brand">
-                <a href="/" class="navbar-item has-icon-left">
-                    <x-icon name="heroicon-s-beaker" size="24" class="has-text-muted" />
+    <nav class="navbar is-transparent">
+        <div class="navbar-brand">
+            <a href="/" class="navbar-item has-icon-left">
+                <x-icon name="heroicon-s-beaker" size="24" class="has-text-muted" />
 
-                    <span class="has-text-body is-uppercase has-text-weight-bold">
-                        {{ config('app.name') }}
-                    </span>
-                </a>
+                <span class="has-text-body is-uppercase has-text-weight-bold">
+                    {{ config('app.name') }}
+                </span>
+            </a>
 
-                <a x-data @click="$store.menu.toggle()" role="button" class="navbar-burger" :class="{ 'is-active': $store.menu.isActive }">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
+            <a x-data @click="$store.menu.toggle()" role="button" class="navbar-burger" :class="{ 'is-active': $store.menu.isActive }">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
 
-            <div x-data id="main-menu" class="navbar-menu" :class="{ 'is-active': $store.menu.isActive }">
-                <div class="navbar-end">
-                        @auth
-                            <div class="navbar-item">
-                                <div>
-                                    <a href="{{ route('dashboard') }}" class="button">
-                                        <x-icon name="heroicon-s-user" size="24" />
+        <div x-data id="main-menu" class="navbar-menu" :class="{ 'is-active': $store.menu.isActive }">
+            <div class="navbar-end">
+                    @auth
+                        <div class="navbar-item">
+                            <div>
+                                <a href="{{ route('dashboard') }}" class="button">
+                                    <x-icon name="heroicon-s-user" size="24" />
+                                </a>
+
+                                @role('admin')
+                                    <a href="{{ route('admin.index') }}" class="button">
+                                        <x-icon name="heroicon-s-lock-closed" size="24" />
                                     </a>
-
-                                    @role('admin')
-                                        <a href="{{ route('admin.index') }}" class="button">
-                                            <x-icon name="heroicon-s-lock-closed" size="24" />
-                                        </a>
-                                    @endrole
-                                </div>
+                                @endrole
                             </div>
+                        </div>
 
-                            <div class="navbar-item">
-                                <form method="post" action="{{ route('logout') }}">
-                                    @csrf
+                        <div class="navbar-item">
+                            <form method="post" action="{{ route('logout') }}">
+                                @csrf
 
-                                    <button class="button">
-                                        {{ __('Logout') }}
-                                    </button>
-                                </form>
-                            </div>
-                        @endauth
+                                <button class="button">
+                                    {{ __('Logout') }}
+                                </button>
+                            </form>
+                        </div>
+                    @endauth
 
-                        @guest
-                            <div class="navbar-item">
-                                <div>
-                                    <a href="{{ route('login') }}" class="button">
-                                        {{ __('Login') }}
+                    @guest
+                        <div class="navbar-item">
+                            <div>
+                                <a href="{{ route('login') }}" class="button">
+                                    {{ __('Login') }}
+                                </a>
+
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="button is-primary">
+                                        {{ __('Register') }}
                                     </a>
-
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}" class="button is-primary">
-                                            {{ __('Register') }}
-                                        </a>
-                                    @endif
-                                </div>
+                                @endif
                             </div>
-                        @endguest
-                    </div>
+                        </div>
+                    @endguest
                 </div>
             </div>
-        </nav>
-    </x-layout.container>
+        </div>
+    </nav>
 </header>
