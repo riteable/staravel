a7613c341b98fc39cebdb1513fe7f5ddc9d86dd9
Riteable <contact@riteable.com>
2021-04-19 14:15:01 +0200

    fix: added toast to components

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,11 @@ import 'alpinejs'
 
 import menuStore from './store/menu'
 import themeStore from './store/theme'
+import toast from './components/toast'
 
 window.Spruce.store('menu', menuStore)
 window.Spruce.store('theme', themeStore)
+
+window.components = {
+  toast
+}
