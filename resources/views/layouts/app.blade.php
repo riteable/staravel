b645fefa9c5091c063b3537f6f1bc96995f46d38
Riteable <contact@riteable.com>
2021-04-26 14:45:35 +0200

    fix: added assets url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="@yield('description')">
 
         <title>@yield('title', 'Index') - {{ config('app.name') }}</title>
 
         <link rel="preconnect" href="https://fonts.gstatic.com">
-
         <link rel="manifest" href="{{ asset('web-manifest.json') }}">
-
         <link rel="icon" href="{{ manifest_icon_search('sizes', '16x16', 'web-manifest.json') }}">
-
-        <livewire:styles />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap">
+
         <x-theme
             id="stylesheet-theme"
             light="{{ manifest_get('theme-light.css', 'assets-manifest.json') }}"
